85e5130819d50df97aaa651e29c999afbed5b817
GamendeMier <mauritsmlt@gmail.com>
2020-06-04 22:03:57 +0200

    fix nested structures and add some comments

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
+-- The window which has nano or any other text editor open
 NANO_WINDOW = arg[1] or os.exit(1)
+
+-- Name of the file which will be retyped
 FILENAME = arg[2] or os.exit(1)
 
+-- The default delay between keypressed (in milliseconds)
 DELAY = 100
 
+-- Search for the index of a value in a table
 local function get_index(t, v)
 	for i, o in ipairs(t) do
 		if o == v then
@@ -13,11 +18,14 @@ local function get_index(t, v)
 	return nil
 end
 
+-- Calls the cmake executable and turns it into orders
+-- Orders contain the start and stop lines
 local function ctags(filename, lines)
 	local handle = io.popen("ctags --fields=+ne -o - "..filename)
 
 	local patterns = {}
 
+	-- Parse the output of ctags into a table
 	for line in handle:lines() do
 		if line:sub(0, 1) ~= '!' then
 			local words = {}
@@ -30,36 +38,60 @@ local function ctags(filename, lines)
 
 	local orders = {}
 
-	for i, words in ipairs(patterns) do
-		orders[i] = {}
+	for _, words in ipairs(patterns) do
+		local order = {}
 		for _, word in ipairs(words) do
 			if word:sub(1, 5) == "line:" then
-				orders[i].start = tonumber(word:sub(6))
-				orders[i].stop = orders[i].start
+				order.start = tonumber(word:sub(6))
+				order.stop = order.start
 			elseif word:sub(1, 4) == "end:" then
-				orders[i].stop = tonumber(word:sub(5))
+				order.stop = tonumber(word:sub(5))
 			end
 		end
-		print(orders[i].start, orders[i].stop)
+		for i, prev in ipairs(orders) do
+			-- If the order is nested within a previous order,
+			-- split it into two seperate orders
+			if prev.start < order.start and order.stop < prev.stop then
+				local new_order = {}
+				new_order.start = prev.start
+				new_order.stop = order.start - 1
+				prev.start = order.stop + 1
+				table.insert(orders, i, new_order)
+			end
+			if order.start < prev.start and prev.stop < order.stop then
+				local new_order = {}
+				new_order.start = order.start
+				new_order.stop = prev.start - 1
+				order.start = prev.stop + 1
+				table.insert(orders, new_order)
+			end
+		end
+		table.insert(orders, order)
 	end
 
+	-- Unread_lines is a table that contains all the line numbers that
+	-- aren't covered by the tags (like comments and #include)
 	local unread_lines = {}
 
+	-- Initialize with all the line numbers
 	for i = 1, #lines do
 		table.insert(unread_lines, i)
 	end
 
+	-- Only keep the line numbers which aren't covered
+	-- (so removing those inside the bounds of an "order")
 	for _, order in ipairs(orders) do
 		for i = order.start, order.stop do
 			local index = get_index(unread_lines, i)
 			if index ~= nil then
 				table.remove(unread_lines, index)
 			else
-				print("overlap", i)
+				error("overlap "..tostring(i))
 			end
 		end
 	end
 
+	-- Prepend the lines to the table (this should be changed later)
 	for i = #unread_lines, 1, -1 do
 		table.insert(orders, 1, { start = unread_lines[i], stop = unread_lines[i] })
 	end
